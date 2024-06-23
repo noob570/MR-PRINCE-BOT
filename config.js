f40dfc6734deace1b8af90c6c0aa073c0d188828
noob570 <anwarhussain4019@gmail.com>
2024-06-23 09:58:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 //💌------------------------------------------💌
 
 
-//💌global.pairingNumber = "" //put your bot number here💌
+//💌global.pairingNumber = "923103443197" //put your bot number here💌
 global.mods = ['923042205427'] 
 global.prems = ['']
 global.allowed = ['']
